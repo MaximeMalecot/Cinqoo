9fa36132f8a777869c281b80ba58eea96414a74a
MaximeMalecot <92maximemalecot@gmail.com>
2023-05-11 15:19:29 +0200

    User prisma fully working

--- user-service/src/app.controller.ts
+++ user-service/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
@@ -6,8 +6,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @EventPattern("getHello")
-  getHello(): string {
+  @EventPattern('getHello')
+  getHello() {
     return this.appService.getHello();
   }
 }
--- user-service/src/app.service.ts
+++ user-service/src/app.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'User service';
+  constructor(private prisma: PrismaService) {}
+
+  getHello() {
+    return this.getUsers();
   }
-  
+
+  async getUsers() {
+    return await this.prisma.user.findMany();
+  }
 }
