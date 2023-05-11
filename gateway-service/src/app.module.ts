dc07969faacc2e0864429516f4e9a6ed29bc1a07
MaximeMalecot <92maximemalecot@gmail.com>
2023-05-11 12:03:27 +0200

    Micro-service communication working

--- gateway-service/src/app.module.ts
+++ gateway-service/src/app.module.ts
@@ -1,9 +1,45 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "AUTH_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "auth-service",
+          port: 3001
+        }
+      },
+      {
+        name: "USER_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "user-service",
+          port: 3002
+        }
+      },
+      {
+        name: "PRESTATION_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "prestation-service",
+          port: 3003
+        }
+      },
+      {
+        name: "REVIEW_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "review-service",
+          port: 3004
+        }
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: "0.0.0.0",
+      port: process.env.PORT || 3000
+    }
+  });
+
+  await app.listen();
 }
+
 bootstrap();
