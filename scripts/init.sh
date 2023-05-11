877799bc43f6f6bc94109bc64b635a2788296a34
julian-saleix <julian.saleix@gmail.com>
2023-05-11 16:32:38 +0200

    Adding migrate script in review/prestation/user service pkg.json + init & migrate scripts to use it

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,1 +1,4 @@
-docker compose up -d --build
+cp review-service/.env.example review-service/.env; \
+cp prestation-service/.env.example prestation-service/.env;  \
+docker compose up -d --build; \
+sh scripts/migrate.sh
---
+++ scripts/migrate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Chemin relatif vers le dossier contenant les projets
+projects_directory=".."
+
+# Récupération du chemin absolu du script
+script_directory="$(cd "$(dirname "$0")" && pwd)"
+
+# Chemin absolu vers le dossier des projets
+full_projects_directory="$script_directory/$projects_directory"
+
+# Fonction pour exécuter le script "migrate"
+execute_migrate_script() {
+  local project_dir=$1
+  local package_file="$project_dir/package.json"
+
+  # Vérification de l'existence du fichier package.json
+  if [[ -f "$package_file" ]]; then
+    # Récupération du contenu de package.json
+    package_content=$(cat "$package_file")
+
+    # Vérification de l'existence du script "migrate" dans package.json
+    if [[ $package_content == *'"migrate":'* ]]; then
+      echo "Exécution du script 'migrate' pour le projet $project_dir."
+      (cd "$project_dir" && npm run migrate)
+    fi
+  fi
+}
+
+# Parcours des projets
+for project_dir in "$full_projects_directory"/*; do
+  if [[ -d "$project_dir" ]]; then
+    execute_migrate_script "$project_dir"
+  fi
+done
