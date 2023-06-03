8969098b4512c8d4f1f9507d99db605895a27c59
MaximeMalecot <92maximemalecot@gmail.com>
2023-06-03 11:45:47 +0200

    Added githubactions test for npm package version

--- gateway-service/src/app.controller.ts
+++ gateway-service/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Req } from '@nestjs/common';
+import { Public } from 'src/auth/decorators/public.decator';
 
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   async getHello(@Req() req): Promise<string> {
     return 'Hello World!';
