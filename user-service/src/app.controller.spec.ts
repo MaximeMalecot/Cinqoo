d21153f92dc4408f862cccee48fe9d3dbf73bb47
jsaleix <julian.saleix@gmail.com>
2023-06-04 22:42:00 +0200

    user-service basic test fixed

--- user-service/src/app.controller.spec.ts
+++ user-service/src/app.controller.spec.ts
@@ -1,22 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, Model, connect } from 'mongoose';
+import { User, UserSchema } from './schema/user.schema';
+import {
+  FreelancerProfile,
+  FreelancerProfileSchema,
+} from './schema/freelancer-profile.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
-describe('AppController', () => {
-  let appController: AppController;
+describe('AppService', () => {
+  let service: AppService;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let userModel: Model<User>;
+  let freelancerModel: Model<FreelancerProfile>;
 
   beforeEach(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    userModel = mongoConnection.model(User.name, UserSchema);
+    freelancerModel = mongoConnection.model(
+      FreelancerProfile.name,
+      FreelancerProfileSchema,
+    );
+
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        { provide: getModelToken(User.name), useValue: userModel },
+        {
+          provide: getModelToken(FreelancerProfile.name),
+          useValue: freelancerModel,
+        },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    service = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return an array', async () => {
+      expect(Array.isArray(await service.getHello())).toBe(true);
     });
   });
 });
