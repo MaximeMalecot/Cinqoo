fe136717d4c3259c6bf0b1febfb2da755e475e55
julian-saleix <julian.saleix@gmail.com>
2023-06-21 17:20:42 +0200

    Adding deliverable routes in gateway contrller with guards

--- gateway-service/src/deliverable/deliverable.controller.ts
+++ gateway-service/src/deliverable/deliverable.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { ROLE } from 'src/auth/enums/role.enum';
+import { CheckObjectIdPipe } from 'src/pipes/checkobjectid.pipe';
+import { IsAllowedToPublish } from './guards/is-allowed-to-publish';
+import { IsAllowedToSeeOrder } from './guards/is-allowed-to-see-order.guard';
 
 @ApiTags('deliverable')
 @Controller('deliverable')
@@ -10,9 +23,29 @@ export class DeliverableController {
     private readonly deliverableService: ClientProxy,
   ) {}
 
-  @Get()
-  public getDeliverableHello() {
-    console.log('getDeliverableHello');
-    return this.deliverableService.send('getHello', {});
+  @UseGuards(IsAllowedToSeeOrder)
+  @Get(':orderId')
+  public getAllDeliverablesForAnOrder(
+    @Param('orderId', CheckObjectIdPipe) orderId: string,
+  ) {
+    // return this.deliverableService.send(
+    //   'DELIVERABLE.GET_ALL_FOR_ORDER',
+    //   orderId,
+    // );
+    return 'not implemented yet';
+  }
+
+  @Post(':orderId')
+  @Roles(ROLE.FREELANCER)
+  @UseGuards(IsAllowedToPublish)
+  public publishDeliverable(
+    @Param('orderId', CheckObjectIdPipe) orderId: string,
+    @Body() body: any,
+  ) {
+    // return this.deliverableService.send('DELIVERABLE.GET_ALL_FOR_ORDER', {
+    //   orderId,
+    //   data: body,
+    // });
+    return 'not implemented yet';
   }
 }
