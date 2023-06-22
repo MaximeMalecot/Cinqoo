230498a2581559ed49dc229d79b486dbbd6e61cd
FranciscoSacha <francisco.leblanc.sacha@gmail.com>
2023-06-22 15:16:29 +0200

    Create and Update Report Reason

--- gateway-service/src/app.module.ts
+++ gateway-service/src/app.module.ts
@@ -24,7 +24,7 @@ import { WebhookModule } from './webhook/webhook.module';
     OrderModule,
     DeliverableModule,
     FavoriteModule,
-    WebhookModule
+    WebhookModule,
   ],
   controllers: [AppController],
 })
--- gateway-service/src/report/report.controller.ts
+++ gateway-service/src/report/report.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { ROLE } from 'src/auth/enums/role.enum';
+import { CheckObjectIdPipe } from 'src/pipes/checkobjectid.pipe';
+import { CreateReportReasonDto } from './dto/create-report-reason.dto';
+import { UpdateReportReasonDto } from './dto/update-report-reason.dto';
 
 @ApiTags('report')
 @Controller('report')
@@ -12,5 +25,34 @@ export class ReportController {
   @Get()
   public getReportHello() {
     return this.reportService.send('getHello', {});
+  }
+
+  @Post()
+  public createReport(@Body() body: CreateReportReasonDto) {
+    return this.reportService.send('REPORT.CREATE', body);
+  }
+
+  @Post('/reasonUser')
+  @Roles(ROLE.ADMIN)
+  public createReportReasonUser(@Body() body: CreateReportReasonDto) {
+    return this.reportService.send('REPORT_REASON_USER.CREATE', body);
+  }
+
+  @Post('/reasonService')
+  @Roles(ROLE.ADMIN)
+  public createReportReasonService(@Body() body: CreateReportReasonDto) {
+    return this.reportService.send('REPORT_REASON_SERVICE.CREATE', body);
+  }
+
+  @Patch('/:reportReasonId')
+  @Roles(ROLE.ADMIN)
+  public updateReportReason(
+    @Param('reportReasonId', CheckObjectIdPipe) reportReasonId: string,
+    @Body() body: UpdateReportReasonDto,
+  ) {
+    return this.reportService.send('REPORT_REASON.UPDATE', {
+      reportReasonId,
+      updateReportReason: body,
+    });
   }
 }
