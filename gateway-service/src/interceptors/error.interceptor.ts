c4ebe18485dc6582abae968782e168ee11ccc484
MaximeMalecot <92maximemalecot@gmail.com>
2023-06-23 11:54:57 +0200

    Added some precision on error logging

--- gateway-service/src/interceptors/error.interceptor.ts
+++ gateway-service/src/interceptors/error.interceptor.ts
@@ -11,12 +11,19 @@ import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable()
 export class CustomErrorInterceptor implements NestInterceptor {
-  private readonly logger: Logger = new Logger();
+  private logger: Logger = new Logger();
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((err) => {
-        this.logger.error(err.message, err, context.getClass().name);
+        this.logger.error(
+          err.message ?? err,
+          err.stack ?? {
+            stack: err.stack,
+            statusCode: err.statusCode,
+          },
+          context.getClass().name,
+        );
 
         if (err instanceof HttpException) {
           return throwError(() => err);
