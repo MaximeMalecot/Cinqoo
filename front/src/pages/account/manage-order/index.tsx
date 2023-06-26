41c1bfdd31c0718a4ff45b57ec5fe8ac1f1c07f7
julian-saleix <julian.saleix@gmail.com>
2023-06-26 23:22:20 +0200

    Adding breadcrumbs

--- front/src/pages/account/manage-order/index.tsx
+++ front/src/pages/account/manage-order/index.tsx
@@ -1,7 +1,24 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function ManageOrder() {
     const { id } = useParams();
 
-    return <div></div>;
+    return (
+        <div className="container mx-auto flex flex-col p-5 md:p-0 md:py-10 gap-5">
+            <div className="text-sm breadcrumbs">
+                <ul>
+                    <li>
+                        <p>Account</p>
+                    </li>
+                    <li>
+                        <Link to="/account/orders">Orders</Link>
+                    </li>
+                    <li>Order #{id}</li>
+                </ul>
+            </div>
+            <div className="flex flex-col md:flex-row justify-between">
+                <h1 className="text-2xl">Manage order</h1>
+            </div>
+        </div>
+    );
 }
