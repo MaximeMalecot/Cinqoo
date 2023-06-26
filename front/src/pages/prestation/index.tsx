3c2710f590475cf07ada146520afdc3da27ecbc7
julian-saleix <julian.saleix@gmail.com>
2023-06-26 11:38:11 +0200

    proceed to payment works

--- front/src/pages/prestation/index.tsx
+++ front/src/pages/prestation/index.tsx
@@ -1,10 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import Button from "../../components/button";
+import { useParams } from "react-router-dom";
 import { useAuthContext } from "../../contexts/auth.context";
 import { PrestationItemList } from "../../interfaces/prestation";
 import prestationService from "../../services/prestation.service";
 import { displayMsg } from "../../utils/toast";
+import OrderBox from "./order-box";
 
 export default function Prestation() {
     const { isConnected } = useAuthContext();
@@ -79,68 +79,7 @@ export default function Prestation() {
                         <h3 className="text-xl font-bold">Rating</h3>
                     </div>
                 </div>
-                <div className="w-full md:w-1/3 h-fit border flex flex-col gap-5 border-1 border-slate-300 rounded-md p-5 overflow-hidden w-full">
-                    <h1 className="text-xl">Order this prestation</h1>
-                    <div className="flex flex-col">
-                        <div className="flex justify-between">
-                            <p>Delay:</p>
-                            <p>{prestation.delay} days</p>
-                        </div>
-                        <div className="flex justify-between">
-                            <p>Revisions included:</p>
-                            <p>{prestation.revisionNb}</p>
-                        </div>
-                        <div className="text-xl font-bold flex justify-between">
-                            <p>Total price:</p>
-                            <p>{prestation.price} $</p>
-                        </div>
-                    </div>
-                    {isConnected ? (
-                        <Button
-                            className="flex-1 w-full"
-                            visual="bordered-primary"
-                        >
-                            Fast order{" "}
-                            <svg
-                                width="15"
-                                height="10"
-                                viewBox="0 0 15 10"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M9.999 9L14 5M14 5L9.999 1M14 5L1 5"
-                                    stroke="white"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
-                        </Button>
-                    ) : (
-                        <Link to="/login">
-                            <Button
-                                className="flex-1 w-full"
-                                visual="bordered-primary"
-                            >
-                                Login first{" "}
-                                <svg
-                                    width="15"
-                                    height="10"
-                                    viewBox="0 0 15 10"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M9.999 9L14 5M14 5L9.999 1M14 5L1 5"
-                                        stroke="black"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                    />
-                                </svg>
-                            </Button>
-                        </Link>
-                    )}
-                </div>
+                <OrderBox prestation={prestation} />
             </section>
         </div>
     );
