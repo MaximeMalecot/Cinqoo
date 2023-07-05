4b7228696317beaf651d2b8aeb8f647b97b1760f
FranciscoSacha <francisco.leblanc.sacha@gmail.com>
2023-07-05 14:35:38 +0200

    add tests in mailer-service

---
+++ mailer-service/src/tests/clients-proxies.ts
@@ -0,0 +1,35 @@
+import { Observable } from 'rxjs';
+
+class ClientProxyMock {
+  public send(pattern: string, data: any): any {
+    const observable = new Observable((observer) => {
+      observer.next({ data: 'test' });
+      observer.complete();
+    });
+    return observable;
+  }
+
+  public emit(pattern: string, data: any): void {}
+
+  public close(): void {}
+}
+class UserService extends ClientProxyMock {
+  public send(pattern: string, data: any): any {
+    switch (pattern) {
+      default:
+        return super.send(pattern, data);
+    }
+  }
+}
+
+class MailerService extends ClientProxyMock {
+  public send(pattern: string, data: any): any {
+    switch (pattern) {
+      default:
+        return super.send(pattern, data);
+    }
+  }
+}
+
+export const MockUserService = new UserService();
+export const MockMailerService = new MailerService();
