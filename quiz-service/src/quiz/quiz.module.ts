264639c4a744430771980368bad80faa83ab94af
julian-saleix <julian.saleix@gmail.com>
2023-07-05 22:35:31 +0200

    QuizService both result & quiz services use forwardRef of each other + adding delete quiz methode

--- quiz-service/src/quiz/quiz.module.ts
+++ quiz-service/src/quiz/quiz.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ResultModule } from 'src/results/result.module';
 import { QuizController } from './quiz.controler';
 import { QuizService } from './quiz.service';
 import { Quiz, QuizSchema } from './schemas/quiz.schema';
@@ -10,6 +11,8 @@ import { Quiz, QuizSchema } from './schemas/quiz.schema';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     MongooseModule.forFeature([{ name: Quiz.name, schema: QuizSchema }]),
+    forwardRef(() => ResultModule),
+
   ],
   controllers: [QuizController],
   providers: [QuizService],
